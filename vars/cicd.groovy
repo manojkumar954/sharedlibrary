68476a2526f0898e7e006000879e38416bbebee7
manojkumar954 <manojregalagadda90@gmail.com>
2026-07-30 11:55:39 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -9,3 +9,7 @@ def deployTomcat(jobname,ip,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
+def gitdownload(repo)
+{
+  git "https://github.com/manojkumar954/${repo}.git" 
+}
