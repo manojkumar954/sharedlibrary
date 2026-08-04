369f8d6a4fe637946e6318a93f90fd38bcbd3fee
manojkumar954 <manojregalagadda90@gmail.com>
2026-08-04 22:39:14 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -5,12 +5,12 @@ def buildArtifact()
 {
   sh 'mvn package'
 }
-def deployTomcat(jobname,ip,context)
+def deployTomcat(ip,context)
 {
-  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+  sh "scp /var/lib/jenkins/workspace/${JOB_NAME}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
 
-def runselenium(jobname)
+def runselenium()
 {
-  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+  sh "java -jar ${WORKSPACE}/testing.jar"
 }
